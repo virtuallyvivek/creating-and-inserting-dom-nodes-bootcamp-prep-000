ba3b508fd5665e7f1b644b02d6b8b79a957484c8
Catherine Straus <vnair611@gmail.com>
2020-06-27 04:25:32 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
-document.createElement(tagName)
 
 var element = document.createElement('div');
 
-element. 
+ 
